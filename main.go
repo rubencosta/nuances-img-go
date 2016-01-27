d2686f211773a87420fa36aae5293a40774bda4e
rubencosta <rmc@nubolab.org>
2016-01-27 21:38:59 +0000

    use imaging to resize image

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package nuancesimg
+package main
 
 import (
 	"log"
@@ -6,6 +6,7 @@ import (
 
 	"golang.org/x/net/context"
 
+	"github.com/disintegration/imaging"
 	pb "github.com/rubencosta/nuances-img-go/proto"
 	"google.golang.org/grpc"
 )
@@ -14,17 +15,29 @@ const (
 	port = ":8888"
 )
 
-//server is used to implement imgresizer.ImgResizer
 type server struct{}
 
 func (s *server) ProcessImg(ctx context.Context, in *pb.ImgUrl) (*pb.ImgUrl, error) {
-	return &pb.ImgUrl{Url: in.Url}, nil
+	log.Printf("received %v", in.Url)
+	img, err := imaging.Open(in.Url)
+	if err != nil {
+		return nil, err
+	}
+	resizedImg := imaging.Resize(img, 400, 400, imaging.Lanczos)
+	newImgPath := in.Url
+	if err := imaging.Save(resizedImg, newImgPath); err != nil {
+		return nil, err
+	}
+	log.Printf("return %v", newImgPath)
+	return &pb.ImgUrl{Url: newImgPath}, nil
 }
 
 func main() {
 	list, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
+	} else {
+		log.Printf("listening on port %v", port)
 	}
 	s := grpc.NewServer()
 	pb.RegisterImgResizerServer(s, &server{})
